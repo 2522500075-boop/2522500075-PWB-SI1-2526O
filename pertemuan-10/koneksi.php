8d0ac23a84b0c38467adc339ec1fbb9f5a2a1889
Khairaan Yamani <2522500075@mahasiswa.atmaluhur.ac.id>
2025-12-05 22:41:25 +0700

    memperbaharui koneksi php

--- pertemuan-10/koneksi.php
+++ pertemuan-10/koneksi.php
@@ -2,10 +2,9 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "db_pwd2025";
-
-$conn = mysqli_conect($host, $user, $pass, $db);
-
+$db = "db_pwd2025";
+$conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
-  die("Koneksi gagal: " . mysqli_conect_error());
+die("Koneksi gagal: " . mysqli_connect_error());
 }
+?>
