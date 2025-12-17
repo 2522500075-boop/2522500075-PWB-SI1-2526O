be665f752b35a972678ee1d63330bfe1efe78f7f
Khairaan Yamani <2522500075@mahasiswa.atmaluhur.ac.id>
2025-12-17 11:32:22 +0700

    menambahkan

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -1,18 +1,17 @@
 <?php
 session_start();
 
-
-
+require 'fungsi.php';
+require 'koneksi.php';
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     $_SESSION['flash_error'] = 'Akses tidak valid.';
     redirect_ke('index.php#contact');
 }
-$nama = bersihkan($_POST['txtNama'] ?? '');
+
+$nama  = bersihkan($_POST['txtNama'] ?? '');
 $email = bersihkan($_POST['txtEmail'] ?? '');
 $pesan = bersihkan($_POST['txtPesan'] ?? '');
-
-
 $errors = [];
 
 if ($nama === '') {
