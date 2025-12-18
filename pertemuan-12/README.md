02fddba4c9230a9974ed23a38bdfe25ec7f468a9
Khairaan Yamani <2522500075@mahasiswa.atmaluhur.ac.id>
2025-12-18 17:27:44 +0700

    menambahkan kolom aksi

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -1,2 +1,29 @@
 ﻿## pertemuan-12
-Deskripsi pertemuan...
+## pertemuan-10
+hari ini jumat 5 desember 2025
+<br>Nim: 2522500075</br>
+<br>Nama: Khairaan Yamani</br>
+<ol>
+    <li>menambahkan koneksi php dipertemuan10serta menambahkan code</li>
+    <li>manambahkan database name di phpMyAdmin dengan nama db_pwd2025</li>
+    <li>menambahkan kode di phpMyAdmin di SQL</li>
+    <li>menambahkan file read.php dan menambahkan code</li>
+    <li>memodif code read.php di file read_inc.php</li>
+    <li>mengganti code di bagian 146 di index.php</li>
+    <li>menghapus code dari baris 133 - 141 di index.php</li>
+    <li>menambahkan localhost ke pertemuan -10</li>
+    <li>mengupdate file fungsi.php</li>
+    <li>menambahkan pesan sukses dan gagal di index.php</li>
+    <li>menambahkan flash message di index.php</li>
+    <li>mengubah data yang ada di txtnama email pesan</li>
+    <li>menambahkan request method di proses.php</li>
+    <li>menggantikan data post trim htmlspecialchars</li>
+    <li>menambahkan validasi</li>
+    <li>menambahkan prepared statement mysqli</li>
+    <li>menambahkan tabel no di read php</li>
+    <li>menambahkan captcha 2+3 di index.php</li>
+    <li>menambahkan kolok created</li>
+    <li>menambahkan field tabel tbl_tamu</li>
+    <li>upgrade localhost</li>
+    <li>menambahkan kolom aksi </li>
+
--- pertemuan-12/localhost(1).sql
+++ pertemuan-12/localhost(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 05, 2025 at 04:21 PM
+-- Generation Time: Dec 17, 2025 at 05:59 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -33,17 +33,22 @@ CREATE TABLE `tbl_tamu` (
   `cid` int(11) NOT NULL,
   `cnama` varchar(100) DEFAULT NULL,
   `cemail` varchar(100) DEFAULT NULL,
-  `cpesan` text
+  `cpesan` text,
+  `dcreated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_tamu`
 --
 
-INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`) VALUES
-(1, 'khairaan yamani', '2522500075@mahasiswa.atmaluhur.ac.id', 'perati woe'),
-(2, 'engrasia masyithah', '2522500074@mahasiswa.atmaluhur.ac.id', 'dk usa ngruce truss'),
-(3, 'Jelisa dewi komaria', '2522500067@mahasiswa.atmaluhur.ac.id', 'cube bediem dikit pacak dk');
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`, `dcreated_at`) VALUES
+(1, 'khairaan yamani', '2522500075@mahasiswa.atmaluhur.ac.id', 'perati woe', '2025-12-17 12:45:19'),
+(2, 'engrasia masyithah', '2522500074@mahasiswa.atmaluhur.ac.id', 'dk usa ngruce truss', '2025-12-17 12:45:19'),
+(3, 'Jelisa dewi komaria', '2522500067@mahasiswa.atmaluhur.ac.id', 'cube bediem dikit pacak dk', '2025-12-17 12:45:19'),
+(4, 'Khairaan yamani', '2522500075@mahasiswa.atmaluhur.ac.id', 'menyalaa', '2025-12-17 12:45:19'),
+(5, 'yan', '2522500075@mahasiswa.atmaluhur.ac.id', 'makasi mesi', '2025-12-17 12:45:19'),
+(6, 'yan', '2522500075@mahasiswa.atmaluhur.ac.id', 'okemantapp', '2025-12-17 12:45:19'),
+(10, 'Kha', '2522500075@mahasiswa.atmaluhur.ac.id', 'hjgwduwgey', '2025-12-17 12:55:17');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +68,7 @@ ALTER TABLE `tbl_tamu`
 -- AUTO_INCREMENT for table `tbl_tamu`
 --
 ALTER TABLE `tbl_tamu`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
