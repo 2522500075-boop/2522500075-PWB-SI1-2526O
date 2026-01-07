5c77ffb5dc33a1cd667f8e53c4b71b058183f7e4
Khairaan Yamani <2522500075@mahasiswa.atmaluhur.ac.id>
2026-01-07 17:26:04 +0700

    menambahkan di kolom 4

--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 require 'koneksi.php';
@@ -5,9 +6,17 @@ require 'fungsi.php';
 
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
 $q = mysqli_query($conn, $sql);
-if (!$q) {
-    die("query error: " . mysqli_error($conn));
+if (!$q){
+    die("Query error:" . mysqli_error($conn));
 }
+?>
+
+<?php
+$flash_sukses = $_SESSION['flash_sukses'] ?? ''; #jika query sukses
+$flash_error  = $_SESSION['flash_error'] ?? ''; #jika ada error
+#bersihkan session ini
+unset($_SESSION['flash_sukses'], $_SESSION['flash_error']);
+?>
 
 <?php if (!empty($flash_sukses)): ?>
     <div style="padding:10px; margin-bottom:10px;
@@ -21,13 +30,11 @@ if (!$q) {
         background:#f8d7da; color:#721c24; border-radius:6px;">
         <?= $flash_error; ?>
     </div>
-<?php endif; 
-?>
+<?php endif; ?>
 
 
 <table border="1" cellpadding="8" cellspacing="0">
 <tr>
-
 <th>No</th>
 <th>Aksi</th>
 <th>ID</th>
@@ -40,8 +47,8 @@ if (!$q) {
 <?php while ($row = mysqli_fetch_assoc($q)): ?>
 <tr>
 
-<td><?= $i++; ?></td>
-<td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a></td>
+<td><?=  $i++; ?></td>
+<td><a href= "edit.php?cid=<?=  (int)$row['cid']; ?>'>Edit</a></td> 
 <td><?= $row['cid']; ?></td>
 <td><?= htmlspecialchars($row['cnama']); ?></td>
 <td><?= htmlspecialchars($row['cemail']); ?></td>
